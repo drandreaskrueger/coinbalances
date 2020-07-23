0552220feb075d7909c4dc792a49470d4aa11b37
Andreas Krueger <coding@AndreasKrueger.de>
2020-07-23 05:14:22 +0200

    binance ~ready

---
+++ src/binance.py
@@ -0,0 +1,68 @@
+'''
+Created on 23 Jul 2020
+
+@author: andreas
+'''
+
+from pprint import pprint
+
+import time, hmac, hashlib
+
+    
+import requests
+# from requests_http_signature import HTTPSignatureAuth as sign
+
+from addAuth import credentials
+
+def parseData(data):
+    """
+    e.g. kick out empty balances
+    """
+    nonempty = [(b['asset'].upper(), 
+                 float(b['free'])+float(b['locked'])
+                 ) 
+                for b in data['balances'] 
+                if (float(b['free']) > 0 or float(b['locked']) > 0)]
+    return dict(nonempty) 
+
+def binance_example_given_by_support(api_key, api_secret, call='account'):
+    """
+    from support ticket:
+    For learning and communication purposes, you might check the attachment for a brief and dirty demo.
+    You might want to check the line, where I signed the query string qs
+    signature = hmac.new(bytes(sc,'latin-1'), msg=bytes(qs,'latin-1'), digestmod=hashlib.sha256).hexdigest().upper()
+    If you have further questions, feel free to contact us at any time.
+    """
+    ts = lambda x: int(1000*x)
+    
+    base = "https://api.binance.com/api/v3/%s?" % call
+    qs = "timestamp=%s"%ts(time.time()) 
+    signature = hmac.new(bytes(api_secret,'latin-1'), msg=bytes(qs,'latin-1'), digestmod=hashlib.sha256).hexdigest().upper()
+    h = {"X-MBX-APIKEY": api_key}
+    c = requests.get(base + qs + "&signature=%s"%signature, headers=h)
+    result = c.json()
+    return result
+    
+
+def binanceWrapped(results, exchange_name="binance"):
+    try:
+        keys=credentials(exchange_name)
+        a, s = keys["API key"], keys["SECRET key"]
+        # print(a), print(s)
+        afn=keys["account friendly name"]
+        # data = testing_README(a, s, 'balance')
+        # data=bitmex_funds(a, s)
+        data=parseData( binance_example_given_by_support(a, s) )
+    except Exception as e:
+        print ("ERROR: (%s) %s ... returning EMPTY answer instead." % (type(e), e))
+        afn, data= "", {}
+        
+    results[exchange_name] = {afn: data}
+
+
+if __name__ == '__main__':
+    results={}
+    binanceWrapped(results)
+    pprint(results)
+
+
