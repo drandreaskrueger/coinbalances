ec59cbc8f6232e5b91b611aba2e3bf8cb727c683
Andreas Krueger <coding@AndreasKrueger.de>
2020-07-28 05:04:42 +0200

    example data, timestring added, HTML table formatting

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ Created on 23 Jul 2020
 
 '''
 
-import os, sys
+import os, sys, datetime
 from io import StringIO
 
 import pandas, bottle # pip3 install pandas bottle
@@ -20,6 +20,9 @@ STATIC = './static/'
 if "src" == os.getcwd().split(os.sep)[-1]:  
     bottle.TEMPLATE_PATH.insert(0, '../views')
     STATIC = '../static/'
+
+
+# the routes for the bottle webserver app:
 
 @route('/home')
 def show_home():
@@ -62,31 +65,54 @@ def thisIsWhereTheMagicHappens():
         print (logging)
     return data
 
-#@route('/credentials')
-#def testing_credentials():
-#    """
-#    just for debugging heroku environment variables
-#    !!! switch off in production !!!
-#    """
-#    import authentication
-#    return authentication.credentials()
+def example_data(inputfile=os.path.join("..", "examples", "coinbalances.csv" )):
+    """
+    only used once, to generate the files in the examples folder
+    """
+    with open(inputfile, "r") as f:
+        data = f.read()
+    df = pandas.read_csv(StringIO(data))
+    df['total'] = df.drop('total', axis=1). sum(axis=1) # recalculate row sum
+    data = df.to_csv(index=False)
+    return data
+# thisIsWhereTheMagicHappens=example_data
+
 
 @route('/coinbalances.csv')
 def csv():
     return thisIsWhereTheMagicHappens()
 
+def timestring():
+    """
+    16 letters datetime string
+    """
+    dt = datetime.datetime.now()
+    dtstr=("%s" % dt)[:16]
+    return dtstr
+
 @route('/coinbalances.pre')
 def pre():
     data = thisIsWhereTheMagicHappens()
-    return template('pre-csv', output=data)
+    return template('pre-csv', output=data, ts=timestring())
 
 @route('/coinbalances.table')
 def table():
+    """
+    HTML table, with a bit of formatting.
+    So many zeroes --> make them have less contrast.  
+    """
     data = thisIsWhereTheMagicHappens()
     df = pandas.read_csv(StringIO(data)) 
-    ht=df.to_html(border=0, index=False) 
-    return template('html-table', output=ht)
-
+    ff=lambda x : "%.8f" % x
+    ht=df.to_html(border=1, classes="tableformat", # layout 
+                  justify='center', # headings 
+                  float_format=ff, # right align with 8 digits
+                  index=False) # drop index from dataframe
+    html_string = template('html-table', output=ht, ts=timestring())
+    # make zeros silver gray:
+    html_with_zeros_grayed = html_string.replace("<td>0.00000000", '<td class="zero">0.00000000')
+    return html_with_zeros_grayed 
+    
 
 @error(404)
 def error404(error):
